b06e27739bbd384a3ca56d989c8eee8df11539f1
Popa Teodor <teodor.popa96@gmail.com>
2024-09-13 10:58:45 +0300

    working on

---
+++ react/src/components/Countries.jsx
@@ -0,0 +1,13 @@
+import { useeEffect } from "react";
+import { useDispatch } from "react-redux";
+import { initializeCountries } from "../services/countriesServices";
+
+const Countries = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(initializeCountries());
+    }, [dispatch]);
+    return <div>Countries will be here</div>
+};
+
+export default Countries; 
